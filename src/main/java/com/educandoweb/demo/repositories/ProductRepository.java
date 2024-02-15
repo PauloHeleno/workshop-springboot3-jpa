345795f2c89eb38c8ae07dd65c483e6f183448fe
PauloHeleno <helenop16@gmail.com>
2024-02-15 11:07:39 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/demo/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/demo/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.educandoweb.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.demo.entities.Category;
 import com.educandoweb.demo.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
